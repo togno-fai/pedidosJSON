2d4e19d9f74a1b6442465a56bec8dc5698ea1e32
togno-fai <lucas.tognozzi@est.fi.uncoma.edu.ar>
2022-04-06 21:03:08 -0300

    parece que arregle el fetch

--- public/scripts.js
+++ public/scripts.js
@@ -1,23 +1,75 @@
-import { Tabla } from "./tabla.js"
-// const url = "http://nodejs.toctocgestion.com/productosconn";
+import { Tabla } from "./tabla.js";
 const url = "http://localhost:5000/producto";
-const productos = fetchProductos(url);
-const tablaActual = [];
 
-async function fetchProductos(url) {
+window.onload = cargarTabla(url);
+document.getElementById("postear").onclick = post;
+document.getElementById("eliminar").onclick = eliminar;
+
+async function cargarTabla(url) {
   await fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/JSON",
     },
-  }).then((res) => res.json())
+  })
+    .then((res) => res.json())
     .then((data) => {
-      let cuerpo = document.getElementById("cuerpo");
-      let tabla = Tabla(data)
+      const cuerpo = document.getElementById("cuerpo");
+      const oldtabla = document.getElementById("tabla");
+      if (oldtabla) cuerpo.removeChild(oldtabla);
+      const tabla = Tabla(data);
       cuerpo.appendChild(tabla);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log("error en cargar tabla"));
 }
-function test2() {
-  alert(`The function 'test2' is executed`);
+
+function eliminar() {
+  const nombre = document.getElementById("nombre").value;
+  if (nombre == "") {
+    window.alert('el campo "nombre" no puede estar vacio');
+  } else {
+    if (window.confirm("seguro que desea eliminar: " + nombre)) {
+      const direccion = url + "/" + nombre;
+      console.log(direccion);
+      fetch(direccion, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log("eliminando: ");
+          console.log(JSON.stringify(res));
+        })
+        .then(cargarTabla(url))
+        .catch((err) => console.log(err));
+    }
+  }
 }
+function post() {
+  const inputs = form.querySelectorAll("input");
+  if (inputs[0].value == "") {
+    alert('el campo "nombre" no puede estar vacio');
+  } else {
+    const postBody = Object.fromEntries(
+      [...inputs].map((i) => [
+        i.id,
+        i.type != "number" ? i.value : i.value == "" ? 0 : i.value,
+      ])
+    );
+    fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postBody),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("posteando: ");
+        console.log(res);
+      })
+      .then(cargarTabla(url))
+      .catch((err) => console.log(err));
+  }
+}
+export { post, eliminar };
